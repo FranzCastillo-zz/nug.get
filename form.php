b6c0269d4aced5a2df8afcafac6e6d42f92f8c7f
Fernanda Esquivel <esq21542@uvg.edu.gt>
2021-09-26 23:53:41 -0600

    Eliminación archivo repetido
    No me di cuenta que ya estaba jeje

--- form.php
+++ form.php
@@ -1,26 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
+<html>
+<meta charset="utf-8">
+<head>
+        <title> Publicar en nug.get </title>
         <!-- BOOTSTRAP IMPLEMENTATION-->
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         <!---->
-        <title>Publicar - nug.get</title>
+        <title>Inicio - nug.get</title>
         <link rel = "stylesheet" href = "css/stylesheet.css">
-    </head>
+</head>
     <body>
         <div class="container-fluid" style="background-color:#FFBF69;height:150px;">
             <a href="index.html"><img src="images/nugget-logo.png" width="500" height="130"></a>
         </div>
         <nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="200">
             <ul class="nav navbar-nav">
-            <li><a href="index.html">Inicio</a></li> 
+            <li class="active"><a href="index.html">Inicio</a></li> 
             <li><a href="solicitar.html">Solicitar</a></li> <!-- Cambiar la extension al momento de implementar php-->
             <li><a href="ofrecer.html">Ofrecer</a></li>
-            <li class="active"><a href="form.php">Publicar</a></li>
+            <li><a href="form.php">Publicar</a></li>
             </ul>
         </nav>
         <div class="container-fluid" style="height:1000"> <!--AQUI VA TODO EL CONTENIDO DE LA PAGINA-->
@@ -28,3 +28,6 @@
         </div>
     </body>
 </html>
+
+<?php
+?>
