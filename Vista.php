403d5e3da2ee1f71eb2a7c6cdcba1a7c1e03ffc5
gusanitor8 <gusanitor8@gmail.com>
2021-10-24 13:42:28 -0600

    Clase Vista
    Se escribieron los métodos para la clase Vista

--- Vista.php
+++ Vista.php
@@ -1,7 +1,35 @@
 <?php
     class Vista{
         public function mostrarPublicaciones($ingresos){ //validar si los ingresos estan vacios if($ingresos != null)
-            
+            //$ingresosCheck = mysqli_num_rows($ingresos);
+
+            if($ingresos != null){
+                while($row = mysqli_fetch_assoc($ingresos)){
+                    $this->well($row["Usuario"], $row["Titulo"], $row["Descripcion"], $row["LinkImagen"],$row["LinkContacto"], $row["FechaDePublicacion"]);
+                }
+            }else{
+                echo("Al chile no hay ingresos xd");
+            }        
         }
+
+        private function well($user, $title, $description, $image,  $contactLink, $date){
+            echo('
+            <div class="rectangle">
+            <section>
+                <h1>' . $title .'</h1>
+                <p>'. $description .'</p>
+
+            <p style="font-size:small;"> Publicado por: ' . $user . ' el ' . $date . ' </p><p>
+            </p>
+            </section>
+            <section>
+                <img src="'. $image .'" alt="">
+            </section>
+            </div>
+
+            ');
+        }
+        
     }
+
 ?>
